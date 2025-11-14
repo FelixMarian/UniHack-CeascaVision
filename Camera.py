cccdb6e7c8b600395fab93fe56521868ccac0a00
FelixMarian <Felix.stoenoiu@student.upt.ro>
2025-11-14 15:50:49 +0200

    First iteration of joint for corner, adaugare captura ctv imagini

--- Camera.py
+++ Camera.py
@@ -1,29 +1,65 @@
 import cv2
+import time
+import os
 
-#Camera 0 (singura)
+
+folder_name = "images"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+    print(f"Director creat: {folder_name}")
+
+#Verificam ca s-a pornit Camera 0
 cap = cv2.VideoCapture(0)
-
-#Verificam ca s-a pornit
 if not cap.isOpened():
-    print("Nu s-a pornit camera")
+ 
+    print("Eroare: Nu s-a putut deschide camera.")
     exit()
 
-print("Camera OK. Apasa q pt a iesi")
+print("Feed live pornit. Apasa 'q' pt iesire.")
 
+print("Captura incepe in 5 secunde...")
+
+total_captures = 5
+captures_taken = 0
+
+first_capture_delay = 5.0
+capture_interval = 2.0
+
+start_time = time.time()
+next_capture_time = start_time + first_capture_delay
 
 while True:
-    #Citim un frame, ret = 1 daca a mers
+    #Afisam feedul
     ret, frame = cap.read()
-
-    #Ret = 0, iesim din while
     if not ret:
-        print("Error: Can't receive frame (stream end?). Exiting ...")
+        
+        print("Eroare: Cadru gol. Se inchide...")
         break
 
-    #Rezultat
-    cv2.imshow('Live feed', frame)
+    current_time = time.time()
 
-    #Vedem la 1ms daca se apasa q
+    #Logica de capturare
+    
+    if (captures_taken < total_captures) and (current_time >= next_capture_time):
+        
+        captures_taken += 1
+        
+        #Salvam imaginea
+        filename = os.path.join(folder_name, f"capture_{captures_taken}.jpg")
+        cv2.imwrite(filename, frame)
+        
+        
+        print(f"Capturat: {filename}")
+        
+        next_capture_time += capture_interval
+        
+        if captures_taken == total_captures:
+            
+            print("Captura completa. Feedul ramane activ.")
+
+    cv2.imshow('Live Feed', frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
 
@@ -31,4 +67,4 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-print("Camera feed stopped.")
+print("Feed oprit.")
